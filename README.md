4f2b64cb202e18965a77db270bdd463c93dd8f48
Mona Elshikh <mona.elshikh@hotmail.com>
2022-07-22 19:28:33 +0200

    phase two- finishing design and adding js code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Expenses-chart-component
- bar chart component
+# Frontend Mentor - Expenses chart component solution
+
+### The challenge
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day’s bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device’s screen size
+- See hover states for all interactive elements on the page
+- **Bonus**: Use the JSON data file provided to dynamically size the bars on the chart
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS 3
+- Flexbox
+- Javascript
+- Mobile-first workflow
+
+## Acknowledgments
+
+- Thanks a lot Frontendmentor for your help and support :)
--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
 //#region Declaration
+let url = "../data/data.json";
+let chartDiv = document.querySelector(".chart");
+let weekDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const dayOfWeek = weekDays[new Date().getDay()];
+//#endregion
 
+//#region  Functions
+// function to get data from json file
+async function getData() {
+    let result = await fetch(url);
+    let data = await result.json();
+    createChart(data);
+}
+// function creates the charts from returned api data
+function createChart(list) {
+    list.forEach(item => {
+        // create the day chart item
+        let dayDiv = document.createElement("div");
+        dayDiv.className = "day";
+        dayDiv.setAttribute("data-day", item.day);
 
+        // create the content span
+        let contentSpan = document.createElement("span");
+        contentSpan.className = "content";
+        contentSpan.style.height = item.amount.toString() + "px";
+        if (item.day === dayOfWeek) {
+            contentSpan.classList.add("active");
+        }
+        // create the amount span
+        let amountSpan = document.createElement("span");
+        amountSpan.className = "amount";
+        amountSpan.innerHTML = `$ ${item.amount}`;
+
+        // add amount to day and day to chart contaainer
+        dayDiv.appendChild(contentSpan);
+        dayDiv.appendChild(amountSpan);
+        chartDiv.appendChild(dayDiv);
+    });
+}
+//#endregion
+
+//#region calling
+getData();
 //#endregion
